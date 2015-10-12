cd085cabcabf4b9ea899dbcb8d663ac22cbca1d6
Bartolomé Sánchez <bsanchezsalado@gmail.com>
2015-10-12 18:39:42 +0200

    Correct wrong comments on main.rs for problem 2

--- p002/src/main.rs
+++ p002/src/main.rs
@@ -1,12 +1,12 @@
 extern crate p002;
 
-// $ time cargo run
-//      Running `target/debug/p001`
-// 233168
+// time cargo run
+//     Running `target/debug/p002`
+// 4613732
 //
-// real	0m0.209s
-// user	0m0.104s
-// sys	0m0.108s
+// real	0m0.194s
+// user	0m0.120s
+// sys	0m0.080s
 fn main() {
     println!("{}", p002::f(4000000));
 }

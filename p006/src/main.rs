1645f163f6c91aea0b49b644282526f51c441719
Bartolomé Sánchez <bsanchezsalado@gmail.com>
2015-11-10 21:26:32 +0100

    Update proper time returned values

--- p006/src/main.rs
+++ p006/src/main.rs
@@ -1,12 +1,12 @@
 extern crate p006;
 
-// $ time cargo run
-//      Running `target/debug/p005`
-// 232792560
+//$ time cargo run
+//      Running `target/debug/p006`
+// 25164150
 //
-// real	0m4.624s
-// user	0m4.524s
-// sys	0m0.108s
+// real	0m0.185s
+// user	0m0.084s
+// sys	0m0.112s
 fn main() {
     println!("{}", p006::f(100));
 }
